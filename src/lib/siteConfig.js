8482550992014da80434a41e0b1c59697963b32c
Lukas Winn <lukaswinn@example.com>
2023-03-03 13:15:11 +0000

    Missing semicolon in siteConfig.js

--- src/lib/siteConfig.js
+++ src/lib/siteConfig.js
@@ -6,10 +6,10 @@ export const SITE_DESCRIPTION = "Lukas Winn's Corner of The Internet";
 export const DEFAULT_OG_IMAGE =
 	'https://user-images.githubusercontent.com/6764957/147861359-3ad9438f-41d1-47c8-aa05-95c7d18497f0.png';
 export const MY_TWITTER_HANDLE = 'lukaswinn';
-export const MY_YOUTUBE = 'https://youtube.com/';
+export const MY_YOUTUBE = 'https://youtube.com';
 export const POST_CATEGORIES = ['Blog']; // Other categories you can consider adding: Talks, Tutorials, Snippets, Podcasts, Notes...
 export const GH_PUBLISHED_TAGS = ['Published'];
-export const GH_TESTING_TAGS = ['Draft']
+export const GH_TESTING_TAGS = ['Draft'];
 
 // auto generated variables
 export const REPO_URL = 'https://github.com/' + GH_USER_REPO;
