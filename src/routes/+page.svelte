d8187d310d5ec4b7c2e3b600054c7ff8f724378e
Lukas Winn <lukaswinn@example.com>
2023-03-04 07:46:09 +0000

    Content changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -34,11 +34,9 @@
 >
 	<div class="flex flex-col-reverse items-start sm:flex-row">
 		<div class="flex flex-col pr-8">
-			<h1 class="mb-3 text-3xl font-bold tracking-tight text-black dark:text-white md:text-5xl">
+			<h1 class="mb-3 text-3xl font-bold tracking-tight text-black dark:text-yelllow-500 md:text-5xl">
 				Lukas Winn
 			</h1>
-			<h1 class="mb-3 text-3xl font-bold tracking-tight text-black dark:text-white md:text-5xl">
-					Lukas Winn
 			<h2 class="mb-4 text-gray-700 dark:text-gray-200">
 				My little Corner of the Internet to share my knowledge and opinions on tech related content with a focus on VMware, Coding and other tech areas.
 				Currently working on <span class="font-semibold"
