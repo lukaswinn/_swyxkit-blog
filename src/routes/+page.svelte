f96e2e7d9c42967899b1ac53df38d454a8d14ceb
Lukas Winn <lukaswinn@example.com>
2023-03-08 05:20:15 +0000

    Reverting changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -35,14 +35,15 @@
 	<div class="flex flex-col-reverse items-start sm:flex-row">
 		<div class="flex flex-col pr-8">
 			
-			<h1 class="mb-3 text-3xl font-bold tracking-tight text-black dark:text-blue-500 md:text-5xl">
+			<h1 class="mb-3 text-3xl font-bold tracking-tight text-black dark:text-yellow-500 md:text-5xl">
 				Lukas Winn
 			</h1>
-			<h2 class="mb-4 text-gray-700 dark:text-gray-200"></h2>
-			My little Corner of the Internet to share my knowledge and opinions on tech related content with a focus on VMware, Coding and other tech areas.
+			<h2 class="mb-4 text-gray-700 dark:text-gray-200">
+				My little Corner of the Internet to share my knowledge and opinions on tech related content with a focus on VMware, Coding and other tech areas.
 				<br />
 				<br /> Currently working on: <span class="font-semibold">
 				Learning about Sveltekit and how I can apply this to my blog...</span>
+			</h2>
 			<br />
 			<br />
 		</div>
