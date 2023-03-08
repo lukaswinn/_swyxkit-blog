e1647e0e5035c4011da7d17c593f8fa18aa4e843
Lukas Winn <lukaswinn@example.com>
2023-03-08 05:54:02 +0000

    Adding in sveltekit adapter for netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import { mdsvex } from 'mdsvex';
 import remarkGithub from 'remark-github';
 import remarkAbbr from 'remark-abbr';
@@ -45,26 +45,7 @@ const config = {
 
 	// Docs: https://github.com/sveltejs/kit/blob/master/packages/adapter-netlify/README.md
 	kit: {
-		adapter: adapter({
-			split: false,
-
-			// nov 2022
-			// if true, will create a Netlify Edge Function rather
-			// than using standard Node-based functions. however, also uses esbuild, which as of nov 2022 has a bug on netlify
-			// https://github.com/sveltejs/kit/issues/7839#issuecomment-1328605300
-
-			// dec 2022 - moved back to true since we're using esbuild again
-			edge: true,
-		}),
-		// https://kit.svelte.dev/docs/configuration#csp
-		// csp: {
-		// 	directives: {
-		// 		'script-src': ['self']
-		// 	},
-		// 	reportOnly: {
-		// 		'script-src': ['self']
-		// 	}
-		// }
+		adapter: adapter()	
 	}
 };
 
